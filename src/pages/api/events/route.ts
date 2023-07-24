7c96478716e18c61b17789430b12c022220f54a9
Timothee Gaultier <tiimogo@gmail.com>
2023-07-24 17:13:34 +0200

    correcting events list

--- src/pages/api/events/route.ts
+++ src/pages/api/events/route.ts
@@ -9,3 +9,20 @@ export async function GET() {
     const events: Event[] = await response.json()
     return NextResponse.json(events, { status: 200 })
 }
+
+export async function DELETE(request: Request) {
+   const { id }: Partial<Event> = await request.json()
+   
+   if (!id) {
+       return NextResponse.json({ message: 'Missing id' }, { status: 400 })
+   }
+
+   await fetch(`${DATA_SOURCE_URL}/${id}`, { method: 'DELETE', 
+   headers: { 
+    'Content-Type': 'application/json',
+    //'API-KEY': API_ROUTES.API_KEY
+ }
+    }
+)
+    return NextResponse.json({ message: `Event ${id} deleted` }, { status: 200 })
+}
--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -2,10 +2,17 @@ import { useState } from "react";
 import { PencilAltIcon, XCircleIcon } from "@heroicons/react/solid";
 import DropdownMenu from "@/components/Dropdowns/DropdownMenu";
 import Paginator from "@/components/Paginators/Paginator";
+import Link from "next/link";
+import { log } from "console";
 
 export const getServerSideProps = async () => {
   const res = await fetch("http://localhost:3001/api/events");
   const events = await res.json();
+
+
+  events.map((event: Event) => {
+    console.log(event);
+  });
   return {
     props: {
       events,
@@ -53,8 +60,17 @@ const EventsPage = ({ events }: EventsPageProps) => {
       text: "Delete",
       Icon: XCircleIcon,
       color: "#10B981",
-      action: () => console.log("Delete"),
-    },
+      // /DELETE /api/events/:id
+// /DELETE /api/events/:id
+action: async (id: number) => {
+  const res = await fetch(`/api/events/${id}`, {
+    method: "DELETE",
+  });
+  if (res.ok) {
+    console.log("Event deleted");
+
+  }
+},    },
   ];
 
   const totalPages = Math.ceil(events.length / itemsPerPage);
@@ -65,6 +81,13 @@ const EventsPage = ({ events }: EventsPageProps) => {
     }
     setCurrentPage(pageNumber);
   };
+function handleDate(date: string) {
+  // french date format : dd/mm/yyyy
+  const day = date.slice(8, 10);
+  const month = date.slice(5, 7);
+  const year = date.slice(0, 4);
+  return `${day}/${month}/${year}`;
+}
 
   function handleTime(time: number) {
     // Divise le temps en heures et minutes
@@ -85,88 +108,70 @@ const EventsPage = ({ events }: EventsPageProps) => {
   };
 
   return (
-    <>
-      {events.length === 0 ? (
-        <div className="flex justify-center mt-5">
-          <p className="text-gray-500 text-lg">No events found.</p>
-        </div>
-      ) : (
-        <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
+    <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
           <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr className="border-b border-gray-200 text-center">
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Name
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Description
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Location
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Type
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Date
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Duration
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Status
-                </th>
-                <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-              {currentEvents.map((event) => (
-                <tr key={event.id} className="hover:bg-gray-100">
-                  <td className="px-6 py-4 text-center align-middle">
-                    {event.name}
-                  </td>
-                  <td className="px-6 py-4 text-center align-middle">
-                    {event.description}
-                  </td>
-                  <td className="px-6 py-4 text-center align-middle">
-                    {event.location}
-                  </td>
-                  <td className="px-6 py-4 text-center align-middle">
-                    {event.type}
-                  </td>
-                  <td className="px-6 py-4 text-center align-middle">
-                    {event.date}
-                  </td>
-                  <td className="px-6 py-4 text-center align-middle">
-                    {handleTime(event.duration)}
-                  </td>
-                  <td className="px-6 py-4 text-center align-middle">
-                    {event.status}
-                  </td>
-                  <td className="px-6 py-4 align-middle text-center">
-                    <DropdownMenu options={options} action={null} />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="flex justify-center mt-4">
-            {events.length > itemsPerPage && (
-              <Paginator
-                currentPage={currentPage}
-                totalPages={totalPages}
-                itemsPerPage={itemsPerPage}
-                onPageChange={handlePageChange}
-                onItemsPerPageChange={handleItemsPerPageChange}
-                items={events}
-              />
-            )}
-          </div>
-        </div>
-      )}
-    </>
+  <thead className="bg-gray-50">
+    <tr className="border-b border-gray-200 text-center">
+      {events.length > 0 && events[0] && Object.keys(events[0]).map((key) => {
+        if (key !== 'image' ) { // Nous excluons le mot de passe pour des raisons de sécurité.
+          return (
+            <th
+              className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle"
+              key={key}
+            >
+              {key}
+            </th>
+          );
+        }
+        return null;
+      })}
+      <th className="px-6 py-4 text-xs font-medium text-gray-500 uppercase tracking-wider align-middle">
+        Actions
+      </th>
+    </tr>
+  </thead>
+  <tbody className="divide-y divide-gray-100 border-t border-gray-100">
+    {currentEvents.map((event) => (
+//      id	name	description	image	location	type	date	duration	status	createdAt	updatedAt
+      <tr key={event.id} className="hover:bg-gray-100">
+        <td className="px-6 py-4 text-center align-middle">{event.id}</td>
+        <td className="px-6 py-4  text-center align-middle">{event.name}</td>
+        <td className="px-6 py-4 text-center align-middle">{event.description}</td>
+        <td className="px-6 py-4 text-center align-middle">{event.location}</td>
+        <td className="px-6 py-4 text-center align-middle">
+          <span className={`inline-flex items-center gap-1 rounded-full px-2 py-1 text-xs font-semibold ${event.type ? 'bg-green-200' : 'bg-red-200'}`}>
+            {event.type ? 'offline' : 'online'}
+          </span>
+        </td>        <td className="px-6 py-4 text-center align-middle">{handleDate(event.date)}</td>
+        <td className="px-6 py-4 text-center align-middle">{handleTime(event.duration)}</td>
+        <td className="px-6 py-4 text-center align-middle">{event.status}</td>
+        <td className="px-6 py-4 text-center align-middle">{handleDate(event.createdAt)}</td>
+        <td className="px-6 py-4 text-center align-middle">{handleDate(event.updatedAt)}</td>
+
+ 
+        {/* Actions (Edit & Delete) */}
+        <td className="px-6 py-4 align-middle text-center">
+          <DropdownMenu options={options}  action={null}/>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+                <div className="flex justify-center mt-4">
+                    {events.length > itemsPerPage && (
+                        <Paginator
+                            currentPage={currentPage}
+                            totalPages={totalPages}
+                            itemsPerPage={itemsPerPage}
+                            onPageChange={handlePageChange}
+                            onItemsPerPageChange={handleItemsPerPageChange}
+                            items={events}
+                        />
+                    )}
+                </div>
+            </div> 
+
+
   );
 };
 
